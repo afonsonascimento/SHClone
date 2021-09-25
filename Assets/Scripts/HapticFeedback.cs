1b6662dee8888c329890c396a60ad0bf5ae52a5a
Afonso <afonso.ferreira.nascimento@gmail.com>
2021-09-25 17:08:18 +0100

    Unity base project with VR sdks

---
+++ Assets/Scripts/HapticFeedback.cs
@@ -0,0 +1,25 @@
+﻿using VRTK;
+
+public class HapticFeedback : VRTK_InteractableObject
+{
+    private VRTK_ControllerReference controllerReference;
+
+    
+    public override void Grabbed(VRTK_InteractGrab grabbingObject)
+    {
+        base.Grabbed(grabbingObject);
+        controllerReference = VRTK_ControllerReference.GetControllerReference(grabbingObject.controllerEvents.gameObject);
+        HapticPulse();
+    }
+
+    public override void Ungrabbed(VRTK_InteractGrab previousGrabbingObject)
+    {
+        base.Ungrabbed(previousGrabbingObject);
+        controllerReference = null;
+    }
+
+    private void HapticPulse()
+    {
+        VRTK_ControllerHaptics.TriggerHapticPulse(controllerReference, 1, 0.1f, 0.01f);
+    }
+}
